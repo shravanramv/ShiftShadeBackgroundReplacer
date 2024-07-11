47694e1ed76c11c26d0c475d3a71b8dceb896381
Shravan Ram V <shravanv.ram@gmail.com>
2024-07-12 00:01:39 +0800

    Changes to the css of the page

--- shiftshade-vip-v2/src/App.js
+++ shiftshade-vip-v2/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <h1 id='mainHeader'>Group 4 Present</h1>
+      </div>
       <div className="content">
         <h1>ShiftShade - Background Shifter</h1>
         <button className="start-button">Start Now!</button>
