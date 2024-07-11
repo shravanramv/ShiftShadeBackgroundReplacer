28fc00685fba4d446b0ad017b88c670e17843737
Shravan Ram V <shravanv.ram@gmail.com>
2024-07-11 23:02:53 +0800

    Initial commit

---
+++ shiftshade-vip-v2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>ShiftShade - Background Shifter</h1>
+        <button className="start-button">Start Now!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
