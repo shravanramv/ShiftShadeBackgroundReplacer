be61b1d6d9b00dd75890f0b3555b2f34a2693a2c
Shravan Ram V <shravanv.ram@gmail.com>
2024-07-12 21:06:04 +0800

    Added about side panel

--- shiftshade-vip-v2/src/App.js
+++ shiftshade-vip-v2/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function togglePanel() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="App">
       <div className="content">
         <h1>ShiftShade - Background Shifter</h1>
         <Button />
       </div>
+      <AboutButton togglePanel={togglePanel} />
+      <SidePanel isOpen={isOpen} togglePanel={togglePanel} />
     </div>
   );
 }
@@ -23,5 +31,32 @@ function Button() {
   );
 }
 
+function AboutButton({ togglePanel }) {
+  return (
+    <button className="about-button" onClick={togglePanel}>
+      About
+    </button>
+  );
+}
+
+function SidePanel({ isOpen, togglePanel }) {
+  return (
+    <>
+      <div className={`side-panel ${isOpen ? 'open' : ''}`}>
+        <div className="panel-content">
+          <h2>This app is brought to you by:</h2>
+          <h3>VIP Group 4 Members</h3>
+          <ul>
+            <li>Lingkeswari</li>
+            <li>Shravan</li>
+            <li>Kishendhra</li>
+            <li>Ndiritu Collins</li>
+          </ul>
+        </div>
+      </div>
+      {isOpen && <div className="overlay" onClick={togglePanel}></div>}
+    </>
+  );
+}
 
 export default App;
